568d3d38aba078241750a735383f5cae333ede11
Youenn Gasc <youenngasc@gmail.com>
2024-10-15 01:30:34 +0100

    refactor : navigation

--- common_functions/navigation.js
+++ common_functions/navigation.js
@@ -6,6 +6,10 @@ function returnToMap2FromPlanet() {
     window.location.href='../menu/carte2.html';
 }
 
+function returnToMenu() {
+    window.location.href='../menu/main.html';
+}
+
 function beginTerre() {
     window.location.href='laby_terre.html';
 }
@@ -32,4 +36,32 @@ function beginFantom() {
 
 function beginRouge() {
     window.location.href='laby_rouge.html';
+}
+
+function goPlanet1() {
+    window.location.href='../planet1/main_terre.html';
+}
+
+function goPlanet2() {
+    window.location.href='../planet2/main_sable.html';
+}
+
+function goPlanet3() {
+    window.location.href='../planet3/main_glace.html';
+}
+
+function goPlanet4() {
+    window.location.href='../planet4/main_roche.html';
+}
+
+function goPlanet5() {
+    window.location.href='../planet5/main_poison.html';
+}
+
+function goPlanet6() {
+    window.location.href='../planet6/main_fantom.html';
+}
+
+function goPlanet7() {
+    window.location.href='../planet7/main_rouge.html';
 }
