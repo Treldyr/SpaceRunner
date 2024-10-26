670c5b51393a1e8f21c1ccc2672b06684733ab5b
Youenn Gasc <youenngasc@gmail.com>
2024-10-26 22:45:22 +0100

    feat : Add the switch power

--- common_functions/powers.js
+++ common_functions/powers.js
@@ -94,6 +94,19 @@ function startStop(numberPower) {
     disablePowerItem("pouv"+numberPower)
 }
 
+function doSwitch(numberPower) {
+    turnTruePowUsed(numberPower)
+    disablePowerItem("pouv"+numberPower)
+    let temp = fromtop1
+    fromtop1 = fromtop2
+    fromtop2 = temp
+    temp = fromleft1;
+    fromleft1 = fromleft2 
+    fromleft2 = temp
+    updateImageGhost()
+    updateImageCharacter("down")
+}
+
 function turnTruePowUsed(numberPower){
     switch(numberPower){
         case 1:
