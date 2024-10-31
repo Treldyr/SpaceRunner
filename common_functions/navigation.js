1a7769fdca283aeb92e85e03b207bd5e72be8295
Youenn Gasc <youenngasc@gmail.com>
2024-10-31 14:12:50 +0000

    refactor : update navigation planets

--- common_functions/navigation.js
+++ common_functions/navigation.js
@@ -18,74 +18,10 @@ function beginMaze() {
     window.location.href='laby.html';
 }
 
-function goPlanet1() {
-    window.location.href='../planet1/main.html';
+function goPlanetFromMap(numberPlanet) {
+    window.location.href='planet'+numberPlanet+'/main.html';
 }
 
-function goPlanet2() {
-    window.location.href='../planet2/main.html';
-}
-
-function goPlanet3() {
-    window.location.href='../planet3/main.html';
-}
-
-function goPlanet4() {
-    window.location.href='../planet4/main.html';
-}
-
-function goPlanet5() {
-    window.location.href='../planet5/main.html';
-}
-
-function goPlanet6() {
-    window.location.href='../planet6/main.html';
-}
-
-function goPlanet7() {
-    window.location.href='../planet7/main.html';
-}
-
-function goPlanet8() {
-    window.location.href='../planet8/main.html';
-}
-
-function goPlanet9() {
-    window.location.href='../planet9/main.html';
-}
-
-function goPlanet1FromMap() {
-    window.location.href='planet1/main.html'
-}
-
-function goPlanet2FromMap() {
-    window.location.href='planet2/main.html'
-}
-
-function goPlanet3FromMap() {
-    window.location.href='planet3/main.html'
-}
-
-function goPlanet4FromMap() {
-    window.location.href='planet4/main.html';
-}
-
-function goPlanet5FromMap() {
-    window.location.href='planet5/main.html';
-}
-
-function goPlanet6FromMap() {
-    window.location.href='planet6/main.html';
-}
-
-function goPlanet7FromMap() {
-    window.location.href='planet7/main.html';
-}
-
-function goPlanet8FromMap() {
-    window.location.href='planet8/main.html';
-}
-
-function goPlanet9FromMap() {
-    window.location.href='planet9/main.html';
+function goPlanet(numberPlanet) {
+    window.location.href='../planet'+numberPlanet+'/main.html';
 }
