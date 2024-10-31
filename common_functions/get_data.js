ae08e2c24fc26df553ac190d6448f225794525ad
Youenn Gasc <youenngasc@gmail.com>
2024-10-31 19:09:24 +0000

    refactor : set and get the data for planet is way simple

--- common_functions/get_data.js
+++ common_functions/get_data.js
@@ -50,79 +50,25 @@ function retrieveAchievements() {
     }
 }
 
-function retrievePlanet1() {
-    var storedData = localStorage.getItem('terre');
+function retrievePlanet(number_planet,name_planet) {
+    // get the data from the local store
+    let nbCoups = " - ";
+    let nbTemps;
+    let stringTemps;
+    let storedData = localStorage.getItem(name_planet);
     if (storedData !== null) {
         var terre = JSON.parse(storedData);
-        coups1 = terre.coups;
-        temps1 = terre.temps;
+        nbCoups = terre.coups;
+        nbTemps = terre.temps;
     }else{
-        document.getElementById("p1").innerHTML = "???";
+        document.getElementById("p"+number_planet).innerHTML = "???";
     }
-}
-
-function retrievePlanet2() {
-    var storedData = localStorage.getItem('tatooine');
-    if (storedData !== null) {
-        var tatoo = JSON.parse(storedData);
-        coups2 = tatoo.coups;
-        temps2 = tatoo.temps;
-    }else{
-        document.getElementById("p2").innerHTML = "???";
+    // Display the data in the page
+    if(nbTemps==null){
+        stringTemps = " - : - ";
+    }else {
+        stringTemps = pad(parseInt(nbTemps / 60)) + " : "+pad(nbTemps % 60);
     }
-}
-
-function retrievePlanet3() {
-    var storedData = localStorage.getItem('hoth');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        coups3 = terre.coups;
-        temps3 = terre.temps;
-    }else{
-        document.getElementById("p3").innerHTML = "???";
-    }
-}
-
-function retrievePlanet4() {
-    var storedData = localStorage.getItem('kamino');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        coups4 = terre.coups;
-        temps4 = terre.temps;
-    }else{
-        document.getElementById("p4").innerHTML = "???";
-    }
-}
-
-function retrievePlanet5() {
-    var storedData = localStorage.getItem('felucia');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        coups5 = terre.coups;
-        temps5 = terre.temps;
-    }else{
-        document.getElementById("p5").innerHTML = "???";
-    }
-}
-
-function retrievePlanet6() {
-    var storedData = localStorage.getItem('fantomia');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        coups6 = terre.coups;
-        temps6 = terre.temps;
-    }else{
-        document.getElementById("p6").innerHTML = "???";
-    }
-}
-
-function retrievePlanet7() {
-    var storedData = localStorage.getItem('sabliereR');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        coups7 = terre.coups;
-        temps7 = terre.temps;
-    }else{
-        document.getElementById("p7").innerHTML = "???";
-    }
+    document.getElementById("c"+number_planet).innerHTML = nbCoups;
+    document.getElementById("t"+number_planet).innerHTML = stringTemps;  
 }
--- common_functions/set_data.js
+++ common_functions/set_data.js
@@ -1,231 +1,3 @@
-function setHighScorePlanet1() {
-    var storedData = localStorage.getItem('terre');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        let newcoups;
-        let newtemps;
-        if( terre.coups > nbCoups ){
-            newcoups = nbCoups;
-        } else {
-            newcoups = terre.coups;
-        }
-        if(terre.temps > totalSeconds){
-            newtemps = totalSeconds;
-        }else{
-            newtemps = terre.temps;
-        }
-        var terre = {
-            coups: newcoups,
-            temps: newtemps
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('terre', dataString);
-    } else {
-        var terre = {
-            coups: nbCoups,
-            temps: totalSeconds
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('terre', dataString);
-    }   
-}
-
-function setHighScorePlanet2() {
-    var storedData = localStorage.getItem('tatooine');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        let newcoups;
-        let newtemps;
-        if( terre.coups > nbCoups ){
-            newcoups = nbCoups;
-        } else {
-            newcoups = terre.coups;
-        }
-        if(terre.temps > totalSeconds){
-            newtemps = totalSeconds;
-        }else{
-            newtemps = terre.temps;
-        }
-        var terre = {
-            coups: newcoups,
-            temps: newtemps
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('tatooine', dataString);
-    } else {
-        var terre = {
-            coups: nbCoups,
-            temps: totalSeconds
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('tatooine', dataString);
-    }   
-}
-
-
-function setHighScorePlanet3() {
-    var storedData = localStorage.getItem('hoth');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        let newcoups;
-        let newtemps;
-        if( terre.coups > nbCoups ){
-            newcoups = nbCoups;
-        } else {
-            newcoups = terre.coups;
-        }
-        if(terre.temps > totalSeconds){
-            newtemps = totalSeconds;
-        }else{
-            newtemps = terre.temps;
-        }
-        var terre = {
-            coups: newcoups,
-            temps: newtemps
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('hoth', dataString);
-    } else {
-        var terre = {
-            coups: nbCoups,
-            temps: totalSeconds
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('hoth', dataString);
-    }   
-}
-
-
-function setHighScorePlanet4() {
-    var storedData = localStorage.getItem('kamino');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        let newcoups;
-        let newtemps;
-        if( terre.coups > nbCoups ){
-            newcoups = nbCoups;
-        } else {
-            newcoups = terre.coups;
-        }
-        if(terre.temps > totalSeconds){
-            newtemps = totalSeconds;
-        }else{
-            newtemps = terre.temps;
-        }
-        var terre = {
-            coups: newcoups,
-            temps: newtemps
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('kamino', dataString);
-    } else {
-        var terre = {
-            coups: nbCoups,
-            temps: totalSeconds
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('kamino', dataString);
-    }   
-}
-
-function setHighScorePlanet5() {
-    var storedData = localStorage.getItem('felucia');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        let newcoups;
-        let newtemps;
-        if( terre.coups > nbCoups ){
-            newcoups = nbCoups;
-        } else {
-            newcoups = terre.coups;
-        }
-        if(terre.temps > totalSeconds){
-            newtemps = totalSeconds;
-        }else{
-            newtemps = terre.temps;
-        }
-        var terre = {
-            coups: newcoups,
-            temps: newtemps
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('felucia', dataString);
-    } else {
-        var terre = {
-            coups: nbCoups,
-            temps: totalSeconds
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('felucia', dataString);
-    }   
-}
-
-
-function setHighScorePlanet6() {
-    var storedData = localStorage.getItem('fantomia');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        let newcoups;
-        let newtemps;
-        if( terre.coups > nbCoups ){
-            newcoups = nbCoups;
-        } else {
-            newcoups = terre.coups;
-        }
-        if(terre.temps > totalSeconds){
-            newtemps = totalSeconds;
-        }else{
-            newtemps = terre.temps;
-        }
-        var terre = {
-            coups: newcoups,
-            temps: newtemps
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('fantomia', dataString);
-    } else {
-        var terre = {
-            coups: nbCoups,
-            temps: totalSeconds
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('fantomia', dataString);
-    }   
-}
-
-function setHighScorePlanet7() {
-    var storedData = localStorage.getItem('sabliereR');
-    if (storedData !== null) {
-        var terre = JSON.parse(storedData);
-        let newcoups;
-        let newtemps;
-        if( terre.coups > nbCoups ){
-            newcoups = nbCoups;
-        } else {
-            newcoups = terre.coups;
-        }
-        if(terre.temps > totalSeconds){
-            newtemps = totalSeconds;
-        }else{
-            newtemps = terre.temps;
-        }
-        var terre = {
-            coups: newcoups,
-            temps: newtemps
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('sabliereR', dataString);
-    } else {
-        var terre = {
-            coups: nbCoups,
-            temps: totalSeconds
-        };
-        var dataString = JSON.stringify(terre);
-        localStorage.setItem('sabliereR', dataString);
-    }   
-}
-
-
 function resetAventure() {
     var playerData = {
         playerUnlock: 1
@@ -240,4 +12,37 @@ function resetAventure() {
     localStorage.setItem('level', dataString);
 
     window.location.href='selectchar.html';
+}
+
+
+function setHighScorePlanet(planet_name) {
+    var storedData = localStorage.getItem(planet_name);
+    if (storedData !== null) {
+        var terre = JSON.parse(storedData);
+        let newcoups;
+        let newtemps;
+        if( terre.coups > nbCoups ){
+            newcoups = nbCoups;
+        } else {
+            newcoups = terre.coups;
+        }
+        if(terre.temps > totalSeconds){
+            newtemps = totalSeconds;
+        }else{
+            newtemps = terre.temps;
+        }
+        var terre = {
+            coups: newcoups,
+            temps: newtemps
+        };
+        var dataString = JSON.stringify(terre);
+        localStorage.setItem(planet_name, dataString);
+    } else {
+        var terre = {
+            coups: nbCoups,
+            temps: totalSeconds
+        };
+        var dataString = JSON.stringify(terre);
+        localStorage.setItem(planet_name, dataString);
+    }   
 }
