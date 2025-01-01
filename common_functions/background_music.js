726399e37198a76cd8d03d3bb0a6a8bd47f71657
Youenn Gasc <youenngasc@gmail.com>
2025-01-01 15:45:43 +0100

    feat : add music planet 4,5,7,8,9,11

--- common_functions/background_music.js
+++ common_functions/background_music.js
@@ -39,7 +39,7 @@ function launchZorathTheme() {
         ZorathMusic.pause();
         ZorathMusic = null;
     }
-    ZorathMusic = new Audio('../../../music/italy.mp3');
+    ZorathMusic = new Audio('../../../music/afternoon.mp3');
     ZorathMusic.loop = true;
     ZorathMusic.volume = retrieveMusicLevel();
     ZorathMusic.currentTime = 0;
