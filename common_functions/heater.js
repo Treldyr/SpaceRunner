88ac7a0f9119af881ef19ac042d6e13d71a43d27
Youenn Gasc <youenngasc@gmail.com>
2025-01-10 03:04:09 +0000

    fix : access data online

---
+++ common_functions/heater.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARH_QHUcw7ZT4Cdc_TaliWG_qm36UY8U8",
+    authDomain: "spacerunner-14429.firebaseapp.com",
+    projectId: "spacerunner-14429",
+    storageBucket: "spacerunner-14429.firebasestorage.app",
+    messagingSenderId: "977679903961",
+    appId: "1:977679903961:web:8220debaa83b783fbd62c8",
+    measurementId: "G-YDWMCPFL4H"
+};
