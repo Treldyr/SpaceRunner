b849f114f23120542cf763352b0ba33962a38921
Youenn Gasc <youenngasc@gmail.com>
2025-03-02 18:20:28 +0100

    fix : miss line 2 levers

--- common_functions/build.js
+++ common_functions/build.js
@@ -318,6 +318,7 @@ function place_blocks_of_levers(numboard,j,column){ // column is 3 for on levers
 
         let theblock = document.createElement('img');
         theblock.setAttribute('src',"img2.png");
+        theblock.setAttribute('style',"position: fixed;top : "+(5*levers[numboard-1][j][column][k][0]+5) + "vh;left : " +(3.5*levers[numboard-1][j][column][k][1]+5)+ "vw;");
         theblock.className = "img_of_laby";
         nbboard = numboard-1
         theblock.setAttribute('id',"additionBlock"+nbboard+"_"+j+"_"+k);
