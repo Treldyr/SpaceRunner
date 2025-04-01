c40aaa785cc95224d550b5e90930339b17d8bdf0
Youenn Gasc <youenngasc@gmail.com>
2025-04-01 22:40:10 +0200

    add : intro mazes planet 18

---
+++ common_functions/movements/mv_all.js
@@ -0,0 +1,202 @@
+// --------------------------------------------------//
+//                                                   //
+//                 LINEAR MOVEMENTS                  //
+//                                                   //
+// --------------------------------------------------//
+
+
+function dest_downward(){
+    if(actual_board<2){
+        if((boards[actual_board][fromtop+1][fromleft]!=2)&&(boards[actual_board][fromtop+1][fromleft]!=15)){
+            fromtop++;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    } else if (actual_board==2){
+        pos_top = fromtop;
+        pos_left = fromleft;
+        let founded = false;
+        while(founded != true)
+        {
+            if(boards[actual_board][pos_top][pos_left]==6){
+                founded = true;
+            }
+            pos_top++;
+        }
+        fromtop = pos_top-2;
+    } else if (actual_board==3){
+        if(boards[actual_board][fromtop+1][fromleft]!=8){
+            fromtop++;
+            playMove();
+        }else{
+            playWallHit()
+        }
+        checkGhost(ghostLeft,fromleft,ghostTop,fromtop)
+    } else if(actual_board==4){
+        let nextcase = boards[actual_board][fromtop+1][fromleft]
+        if ((nextcase!=12) && (((isDimRed)&&(nextcase!=11))||((!isDimRed)&&(nextcase!=10)))){
+            fromtop++;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    } else if(actual_board==5){
+        if(boards[actual_board][fromtop+1][fromleft]!=14){
+            fromtop++;
+            playMove();
+            light_down(actual_board,fromtop,fromleft)
+        }else{
+            playWallHit()
+        }
+    }
+    document.getElementById('character'+actual_board).setAttribute('src',"../../../images/"+chara+"down.png");
+}
+
+
+function dest_leftward(){
+    if(actual_board<2){
+        if((boards[actual_board][fromtop][fromleft-1]!=2)&&(boards[actual_board][fromtop][fromleft-1]!=15)){
+            fromleft--;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    } else if (actual_board==2){
+        pos_top = fromtop;
+        pos_left = fromleft;
+        let founded = false;
+        while(founded != true)
+        {
+            if(boards[actual_board][pos_top][pos_left]==6){
+                founded = true;
+            }
+            pos_left--;
+        }
+        fromleft = pos_left+2;
+    } else if (actual_board==3){
+        if(boards[actual_board][fromtop][fromleft-1]!=8){
+            fromleft--;
+            playMove();
+        }else{
+            playWallHit()
+        }
+        checkGhost(ghostLeft,fromleft,ghostTop,fromtop)
+    } else if(actual_board==4){
+        let nextcase = boards[actual_board][fromtop][fromleft-1]
+        if ((nextcase!=12) && (((isDimRed)&&(nextcase!=11))||((!isDimRed)&&(nextcase!=10)))){
+            fromleft--;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    } else if(actual_board==5){
+        if(boards[actual_board][fromtop][fromleft-1]!=14){
+            fromleft--;
+            playMove();
+            light_left(actual_board,fromtop,fromleft)
+        }else{
+            playWallHit()
+        }
+    }
+    document.getElementById('character'+actual_board).setAttribute('src',"../../../images/"+chara+"left.png");
+}
+
+
+function dest_upward(){
+    if(actual_board<2){
+        if((boards[actual_board][fromtop-1][fromleft]!=2)&&(boards[actual_board][fromtop-1][fromleft]!=15)){
+            fromtop--;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    }else if (actual_board==2){
+        pos_top = fromtop;
+        pos_left = fromleft;
+        let founded = false;
+        while(founded != true)
+        {
+            if(boards[actual_board][pos_top][pos_left]==6){
+                founded = true;
+            }
+            pos_top--;
+        }
+        fromtop = pos_top+2;
+    } else if (actual_board==3){
+        if(boards[actual_board][fromtop-1][fromleft]!=8){
+            fromtop--;
+            playMove();
+        }else{
+            playWallHit()
+        }
+        checkGhost(ghostLeft,fromleft,ghostTop,fromtop)
+    } else if(actual_board==4){
+        let nextcase = boards[actual_board][fromtop-1][fromleft]
+        if ((nextcase!=12) && (((isDimRed)&&(nextcase!=11))||((!isDimRed)&&(nextcase!=10)))){
+            fromtop--;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    } else if(actual_board==5){
+        if(boards[actual_board][fromtop-1][fromleft]!=14){
+            fromtop--;
+            playMove();
+            light_up(actual_board,fromtop,fromleft)
+        }else{
+            playWallHit()
+        }
+    }
+    document.getElementById('character'+actual_board).setAttribute('src',"../../../images/"+chara+"up.png");
+}
+
+
+function dest_rigthward(){
+    if(actual_board<2){
+        if((boards[actual_board][fromtop][fromleft+1]!=2)&&(boards[actual_board][fromtop][fromleft+1]!=15)){
+            fromleft++;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    } else if (actual_board==2){
+        pos_top = fromtop;
+        pos_left = fromleft;
+        let founded = false;
+        while(founded != true)
+        {
+            if(boards[actual_board][pos_top][pos_left]==6){
+                founded = true;
+            }
+            pos_left++;
+        }
+        fromleft = pos_left-2;
+    } else if (actual_board==3){
+        if(boards[actual_board][fromtop][fromleft+1]!=8){
+            fromleft++;
+            playMove();
+        }else{
+            playWallHit()
+        }
+        checkGhost(ghostLeft,fromleft,ghostTop,fromtop)
+    } else if(actual_board==4){
+        let nextcase = boards[actual_board][fromtop][fromleft+1]
+        if ((nextcase!=12) && (((isDimRed)&&(nextcase!=11))||((!isDimRed)&&(nextcase!=10)))){
+            fromleft++;
+            playMove();
+        }else{
+            playWallHit()
+        }
+    } else if (actual_board==5){
+        if(boards[actual_board][fromtop][fromleft+1]!=14){
+            fromleft++;
+            playMove();
+            light_right(actual_board,fromtop,fromleft)
+        }else{
+            playWallHit()
+        }
+    }
+    document.getElementById('character'+actual_board).setAttribute('src',"../../../images/"+chara+"right.png");
+}
+
