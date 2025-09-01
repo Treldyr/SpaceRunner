7ed636b036fc4c0f79552ab7b4964269678dbb58
Youenn Gasc <youenngasc@gmail.com>
2025-09-01 14:54:02 +0200

    Fix : Bug teleportation end level with powspeed

--- multiplayer/adventure/javascript/lever.js
+++ multiplayer/adventure/javascript/lever.js
@@ -2,49 +2,49 @@ function set_new_positions_with_speedpow(e,height,length){
     if(!inpause){
         if ( is_input_up_p2_multiplayer(e)&&(!p1_won)) { // going up p1 - Z key
             dest_upward_lever_p1(height,length)
-            if(p1_speed){
+            if((p1_speed)&&(!p1_won)){
                 dest_upward_lever_p1(height,length)
             }
         }
         else if (is_input_down_p2_multiplayer(e)&&(!p1_won))  { // going down p1 - S key
             dest_downward_lever_p1(height,length);
-            if(p1_speed){
+            if((p1_speed)&&(!p1_won)){
                 dest_downward_lever_p1(height,length);
             }
         }
         else if (is_input_left_p2_multiplayer(e)&&(!p1_won))  { // going left p1 - Q key
             dest_leftward_lever_p1(height,length);
-            if(p1_speed){
+            if((p1_speed)&&(!p1_won)){
                 dest_leftward_lever_p1(height,length);
             }
         }
         else if (is_input_right_p2_multiplayer(e)&&(!p1_won))  { // going rigth p1 - D key
             dest_rightward_lever_p1(height,length);
-            if(p1_speed){
+            if((p1_speed)&&(!p1_won)){
                 dest_rightward_lever_p1(height,length);
             }
         }
         else if ((e.keyCode == '38') && (!p2_won)) { // going up for p2
             dest_upward_lever_p2(height,length);
-            if(p2_speed){
+            if((p2_speed)&&(!p2_won)){
                 dest_upward_lever_p2(height,length);
             }
         } 
         else if ((e.keyCode == '40') && (!p2_won)) { // going down for p2
             dest_downward_lever_p2(height,length);
-            if(p2_speed){
+            if((p2_speed)&&(!p2_won)){
                 dest_downward_lever_p2(height,length);
             }
         } 
         else if ((e.keyCode == '37') && (!p2_won)) { // going left for p2
             dest_leftward_lever_p2(height,length);
-            if(p2_speed){
+            if((p2_speed)&&(!p2_won)){
                 dest_leftward_lever_p2(height,length);
             }
         } 
         else if ((e.keyCode == '39') && (!p2_won)) { // going right for p2
             dest_rightward_lever_p2(height,length);
-            if(p2_speed){
+            if((p2_speed)&&(!p2_won)){
                 dest_rightward_lever_p2(height,length);
             }
         }
